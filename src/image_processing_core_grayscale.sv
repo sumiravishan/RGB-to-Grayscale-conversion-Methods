b3db74f5e7e495279ce65a61294a627ecd3ee7d1
sumiravishan <99382520+sumiravishan@users.noreply.github.com>
2023-05-24 23:05:30 +0530

    Update image_processing_core_grayscale.sv

--- src/image_processing_core_grayscale.sv
+++ src/image_processing_core_grayscale.sv
@@ -1,3 +1,29 @@
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+////  Author: Sumira Fernando                                    ////
+////          k.w.s.v.fernando@gmail.com                         ////
+////                                                             ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+//// Copyright (C) 2023                                          ////
+////                                                             ////
+//// This source file may be used and distributed without        ////
+//// restriction provided that this copyright statement is not   ////
+//// removed from the file and that any derivative work contains ////
+//// the original copyright notice and the associated disclaimer.////
+////                                                             ////
+//// This source file is free software; you can redistribute it  ////
+//// and/or modify it under the terms of the GNU Lesser General  ////
+//// Public License as published by the Free Software Foundation.////
+////                                                             ////
+//// This source is distributed in the hope that it will be      ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied  ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     ////
+//// PURPOSE.  See the GNU Lesser General Public License for more////
+//// details. http://www.gnu.org/licenses/lgpl.html              ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
 module image_processing_core_grayscale  
 import apb_design_pkg::*, core_design_pkg::*;
 (input PCLK, input PRESETn, input [apb_ADDR_WIDTH-1:0] PADDR [apb_interface_amount-1:0], input [apb_PROT_WIDTH-1:0] PPROT[apb_interface_amount-1:0], 
